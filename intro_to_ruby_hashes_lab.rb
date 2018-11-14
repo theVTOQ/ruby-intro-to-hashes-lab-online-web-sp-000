c3060d391bc822be401222762852c497d17db23d
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-14 00:43:40 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,9 @@ def monopoly_with_second_tier
       pieces: 4,
       names: {
         reading_railroad ={},
-        pennsylvania_railroad = {}
+        pennsylvania_railroad = {},
+        b_and_o_railroad = {},
+        shortline = {}
       },
       rent_in_dollars: {
         one_piece_owned: 25,

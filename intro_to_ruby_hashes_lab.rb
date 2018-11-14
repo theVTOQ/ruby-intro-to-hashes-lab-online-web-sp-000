b6bea4e78d4cef1afced3b47df641313820fd069
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-14 00:40:17 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,9 @@ def monopoly_with_second_tier
       pieces: 4,
       names: {},
       rent_in_dollars: {
-        one_piece_owned: 25
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100
       },
     }
   }
